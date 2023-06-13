fdc340b361754433accbe220be11acf7f70ca295
Yanghwa Kim <doitykim@gmail.com>
2023-06-13 20:53:46 +0900

    update 2023-06-13T20:53

---
+++ 3.storage/300.unload_modules-v1.ps1
@@ -0,0 +1,16 @@
+#
+# Unload module
+#
+
+. "$HOME/scripts/globalval.ps1"
+#
+
+foreach ($f in (import-csv -path $TgtFile))
+{
+	echo $f.alias
+	$esxcli = Get-Esxcli -vmhost $f.alias 
+	$esxcli.system.module.set($false, $false, $unload_module1 ) 
+	$esxcli.system.module.set($false, $false, $unload_module2 ) 
+}
+
+Disconnect-VIServer -Server * -Force -confirm:$false
--- 3.storage/300.unload_modules.ps1
+++ 3.storage/300.unload_modules.ps1
@@ -9,8 +9,8 @@ foreach ($f in (import-csv -path $TgtFile))
 {
 	echo $f.alias
 	$esxcli = Get-Esxcli -vmhost $f.alias -V2
-	$esxcli.system.module.set($false, $false, $unload_module1 ) 
-	$esxcli.system.module.set($false, $false, $unload_module2 ) 
+	$esxcli.system.module.set.Invoke(@{module=$unload_module1; force=$false; enabled=$false})
+	$esxcli.system.module.set.Invoke(@{module=$unload_module2; force=$false; enabled=$false})
 }
 
 Disconnect-VIServer -Server * -Force -confirm:$false
