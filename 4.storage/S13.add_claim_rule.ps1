c2c5b80257641c8ac7ad2c63ec54fcabfaa3589f
Yanghwa Kim <doitykim@gmail.com>
2023-06-15 11:24:08 +0900

    update 2023-06-15

--- 4.storage/S13.add_claim_rule.ps1
+++ 4.storage/S13.add_claim_rule.ps1
@@ -8,7 +8,7 @@ foreach ($f in (import-csv -path $TgtFile))
 {
 	echo $f.ip
 	$esxcli = Get-Esxcli -vmhost $f.alias -V2
-	$esxcli.storage.nmp.satp.rule.add.invoke(@{vendor="DGC"; satp="VMW_SATP_ALUA_CX"; psp="VMW_PSP_RR"; pspoption="iops=1"; claimoption = "tpgs_on"})
+	$esxcli.storage.nmp.satp.rule.add.invoke(@{vendor="DGC"; satp="VMW_SATP_ALUA_CX"; psp="VMW_PSP_RR"; pspoption="iops=1"; claimoption="tpgs_on"})
 #	$esxcli.storage.nmp.satp.rule.remove.invoke(@{vendor="DGC"; satp="VMW_SATP_ALUA_CX"; psp="VMW_PSP_RR"; pspoption="iops=1"; claimoption = "tpgs_on"})
 }
 Disconnect-VIServer -Server * -Force -confirm:$false
