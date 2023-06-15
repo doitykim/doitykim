4a0fda981529011d03adeb83f3319f967e401d42
Yanghwa Kim <doitykim@gmail.com>
2023-06-15 17:09:37 +0900

    update 2023-06-15T17:09

--- 3.network/N04.set_jumbo_vmk.ps1
+++ 3.network/N04.set_jumbo_vmk.ps1
@@ -7,9 +7,9 @@
 foreach ($f in (import-csv -path $TgtFile))
 {
 	echo $f.alias
-	$esxcli = Get-Esxcli -vmhost $f.alias
-	$esxcli.network.ip.interface.set("true", "vmk2", "9000")
-	$esxcli.network.ip.interface.set("true", "vmk3", "9000")
-	$esxcli.network.vswitch.standard.set("listen", "9000", "vSwitch1")
+	$esxcli = Get-Esxcli -vmhost $f.alias -V2
+	$esxcli.network.ip.interface.set.invoke(@{interfacename="vmk2"; enabled=$true; mtu="9000"}))
+	$esxcli.network.ip.interface.set.invoke(@{interfacename="vmk3"; enabled=$true; mtu="9000"}))
+	$esxcli.network.vswitch.standard.set.invoke(@{cdpstatus="listen"; mtu="9000", vswitchname="vSwitch1"})
 }
 Disconnect-VIServer -Server * -Force -confirm:$false
