94204459ca616480410e3e94ddc7c1681510628d
Yanghwa Kim <doitykim@gmail.com>
2023-06-15 17:38:12 +0900

    update 2023-06-15T17:38

--- 4.storage/S06.bind_iscsi_kernel.ps1
+++ 4.storage/S06.bind_iscsi_kernel.ps1
@@ -10,14 +10,9 @@
 
 foreach ($f in (import-csv -path $TgtFile))
 {
-        $iscsi_adapter= Get-VMhost $f.alias |Get-VMHostHba -Type IScsi | Where {$_.Model -eq "iSCSI Software Adapter"}
-        $iscsi_vmk1 = Get-VMHostNetworkAdapter -PortGroup service-for-iscsi1 |select -ExpandProperty Devicename -first 1
-        $iscsi_vmk2 = Get-VMHostNetworkAdapter -PortGroup service-for-iscsi2 |select -ExpandProperty Devicename -first 1
-
         echo $f.alias
         $esxcli = Get-Esxcli -vmhost $f.alias -V2
         $esxcli.iscsi.networkportal.add.invoke(@{adapter=$iscsi_adapter; nic=$iscsi_vmk1; force=$true})
         $esxcli.iscsi.networkportal.add.invoke(@{adapter=$iscsi_adapter; nic=$iscsi_vmk2; force=$true})
-
 }
 Disconnect-VIServer -Server * -Force -confirm:$false
