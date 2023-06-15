99a3a5e7caa203e526e45db97628ecee20e282ae
Yanghwa Kim <doitykim@gmail.com>
2023-06-15 14:42:02 +0900

    update v3

--- 3.network/N01.add_vswitch_VLAN.ps1
+++ 3.network/N01.add_vswitch_VLAN.ps1
@@ -9,7 +9,7 @@ foreach ($f in (import-csv -path $TgtFile))
 	echo $f.alias
 	#vmotion port group
 	New-VMHostNetworkAdapter -VMHost $f.alias -VirtualSwitch vSwitch0 -PortGroup $vmotion_pg  -IP $f.vmotion -SubnetMask 255.255.254.0 -VMotionEnabled $true
-	Get-VirtualPortGroup -Name $vmotion_pg -VMHost $f.alias|set-virtualportgroup -VlanID $mgmt_pg_vlan
+#	Get-VirtualPortGroup -Name $vmotion_pg -VMHost $f.alias|set-virtualportgroup -VlanID $mgmt_pg_vlan
 	# 
 	New-VirtualSwitch -VMHost $f.alias -Name vSwitch1 -Nic vmnic4,vmnic5 -Mtu 9000
 	Get-VMhost $f.alias|Get-VirtualSwitch -Name vSwitch1|New-VirtualPortGroup -Name $service_pg -VlanID $service_pg_vlan
