1b2003d287d0cbff918a502990df2896626ec40b
Yanghwa Kim <doitykim@gmail.com>
2023-06-16 12:52:46 +0900

    v1

--- globalval.ps1
+++ globalval.ps1
@@ -4,7 +4,7 @@
 # vCenter 정보와 작업 대상 ESXi 서버에 사용될 정보를 저장한 파일을 지정한다.
 #
 #
-$TgtFile = "$HOME/scripts/0.target/tgt_g.csv"
+$TgtFile = "$HOME/scripts/0.target/tgt_gs.csv"
 $VcInfoFile = "$HOME/scripts/0.target/vc_vc70.csv"
 
 # ntp server
