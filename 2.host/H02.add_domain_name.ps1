fefaea2c3081bea18d09710bf3514578b2a81028
Yanghwa Kim <doitykim@gmail.com>
2023-06-18 19:21:14 +0900

    Update:2023-06-18T19:21

--- 2.host/H02.add_domain_name.ps1
+++ 2.host/H02.add_domain_name.ps1
@@ -10,6 +10,6 @@ foreach ($f in (import-csv -path $TgtFile))
 	$vmHostNetworkInfo = Get-VmHostNetwork -Host $f.alias
 	#Set-VmHostNetwork -Network $vmHostNetworkInfo -DomainName $domain_name -Dnsaddress $dns_svr
 	#Set-VmHostNetwork -HostName $f.hname -Network $vmHostNetworkInfo -DomainName $domain_name -Dnsaddress $dns_svr
-	Set-VmHostNetwork -HostName $f.hname -Network $vmHostNetworkInfo -DomainName $domain_name  -Dnsaddress $dns_svr
+	Set-VmHostNetwork -HostName $f.hname -Network $vmHostNetworkInfo -DomainName $domain_name  -Dnsaddress $dns_svr -IPv6Enabled $false
 }
 Disconnect-VIServer -Server * -Force -confirm:$false
