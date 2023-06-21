03f6c097f125b68678091e78d3f2a79c2d919282
Yanghwa Kim <doitykim@gmail.com>
2023-06-21 15:11:14 +0900

    Update:2023-06-21T15:31

--- 4.storage/S18.set_delayed_ack_disable-v2.ps1
+++ 4.storage/S18.set_delayed_ack_disable-v2.ps1
@@ -8,12 +8,16 @@ foreach ($f in (import-csv -path $TgtFile))
 {
 	echo $f.alias
 	$esxcli = Get-Esxcli -vmhost $f.alias -V2
-}
-
-$targets = $esxcli.iscsi.adapter.target.portal.list.invoke()  | where-object {$_.Target -like "*purestorage*"}
-
-foreach ($a in ($targets))
-{
-	$esxcli.iscsi.adapter.discovery.sendtarget.param.set.invoke(@{adapter=$iscsi_adapter; address=$a.IP; vaule="false"})
-}
+	$targets = $esxcli.iscsi.adapter.target.portal.list.invoke()  | where-object {$_.Target -like "*freenas*"}
+	$iscsihba = get-vmhosthba |where-object {$_.Model -eq "iSCSI Software Adapter"}
+	foreach ($target in $targets)
+	{
+                   $iscsiargs = $esxcli.iscsi.adapter.discovery.sendtarget.param.set.CreateArgs()
+                   $iscsiargs.adapter = $iscsihba.Device
+                   $iscsiargs.address = $target.IP
+		   $iscsiargs.value = "false"
+                   $iscsiargs.key = "DelayedAck"
+                   $esxcli.iscsi.adapter.discovery.sendtarget.param.set.invoke($iscsiargs) |out-null
+         }     
+}	
 Disconnect-VIServer -Server * -Force -confirm:$false
